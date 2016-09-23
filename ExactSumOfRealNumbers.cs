f3496f21cb92daa11d5b1011100716f910f89e23
Bojidar-At <registri@abv.bg>
2016-09-23 14:17:26 +0300

    03. ExactSumOfRealNumbers.cs

---
+++ ExactSumOfRealNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Exact_Sum_of_Real_Numbers
+{
+    class ExactSumOfRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                decimal a = decimal.Parse(Console.ReadLine());
+                sum += a;
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
