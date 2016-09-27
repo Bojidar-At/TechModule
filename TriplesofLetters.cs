bf8c59434955f491ba87bc385a9d4b9dbc7d4aea
Bojidar-At <registri@abv.bg>
2016-09-27 19:16:45 +0300

    06. TriplesofLetters.cs

---
+++ TriplesofLetters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Triples_of_Letters
+{
+    class TriplesofLetters
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        Console.Write(Convert.ToChar(i + 97));
+                        Console.Write(Convert.ToChar(j + 97));
+                        Console.WriteLine(Convert.ToChar(k + 97));
+
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}
