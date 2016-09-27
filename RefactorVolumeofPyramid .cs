a1fc119d2e5fa6b2f390590435b39a45cb43714d
Bojidar-At <registri@abv.bg>
2016-09-27 19:41:26 +0300

    8. RefactorVolumeofPyramid .cs

---
+++ RefactorVolumeofPyramid .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lendth, width, height = 0;
+            Console.Write("Length: ");
+            lendth = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            height = double.Parse(Console.ReadLine());
+            double volume = (lendth*width*height) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+
+        }
+    }
+}
